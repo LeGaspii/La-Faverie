f955a1ae0589726adb315099c1360cd5e71125cb
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-03-15 15:44:04 +0100

    seeding de base

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+faverie = House.new(name: "La Faverie", address: "La Faverie, 53400 Athée")
+faverie.save!
+p "House ok !!"
+room = Room.new(name: "Chambre bleue", house_id: 1)
+room.save!
+p "room ok !!"
+
+d1 = DateTime.new(2021, 6, 30)
+proj = Project.new(deadline: d1,house_id: faverie, name: "Chasse au dahut", budget: 600, content: "Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?")
+proj.save
+p "project ok"
