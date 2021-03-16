9fbdfd19be663b33a3a015d94c48b64ac7e7c8aa
Bryan Nakache <nakachebryan@gmail.com>
2021-03-16 13:16:02 +0100

    views rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,9 +3,8 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 
-  def create
+ def create
     @room = Room.new(room_params)
-    @room.user = current_user
     if @room.save
       redirect_to room_path(@room)
     else
@@ -13,20 +12,13 @@ class RoomsController < ApplicationController
     end
   end
 
+
   def show
     @room = Room.find(params[:id])
-    @reservation = Reservation.new
-    allusers = User.all
-    @users = allusers.reject do |user|
-      user.reservations.any? do |reservation|
-        reservation.room == @room
-      end
-    end
   end
 
   def index
     @rooms = Room.all
-    @reservations = Reservation.all
   end
 
   private
