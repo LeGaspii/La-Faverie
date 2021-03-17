3a40e86258ac85e3f5aa18231910038de839ec1c
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-03-17 23:04:13 +0100

    usersprojects controller

---
+++ app/controllers/usersprojects_controller.rb
@@ -0,0 +1,2 @@
+class UsersprojectsController < ApplicationController
+end
---
+++ test/controllers/usersprojects_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UsersprojectsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
