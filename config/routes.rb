784ea47470b1a0f75c55b6f1609d4165f57a96fb
Bryan Nakache <nakachebryan@gmail.com>
2021-03-17 11:44:21 +0100

    routes nest pour perks

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :rooms do
     resources :perks, only: [:new, :create, :edit, :destroy, :update, :index ]
+  end
   resources :projects
 end
