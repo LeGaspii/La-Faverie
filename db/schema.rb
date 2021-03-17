8bd03ebffc972e1567b7e7987b1e38a6f8bf0463
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-03-17 18:22:03 +0100

    change value to float and new home

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_100250) do
+ActiveRecord::Schema.define(version: 2021_03_17_171725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2021_03_16_100250) do
     t.bigint "project_id", null: false
     t.string "name"
     t.integer "number"
-    t.integer "value"
+    t.float "value"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["project_id"], name: "index_buyings_on_project_id"
