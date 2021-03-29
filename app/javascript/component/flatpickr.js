4d0d5a52b13da49f0344a1dc9eb4ed9186a2035b
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-03-29 13:53:31 +0200

    test pour eviter le chevauchement des reza

--- app/javascript/component/flatpickr.js
+++ app/javascript/component/flatpickr.js
@@ -2,39 +2,19 @@ import { flatpickr } from '../component/flatpickr';
 
 const startDateInput = document.getElementById('reservation_date_in');
 const endDateInput = document.getElementById('reservation_date_out');
+console.log(startDateInput)
+console.log(endDateInput)
 
-// flatpickr("[data-behavior='flatpickr']", {
-//   altInput: true,
-//   altFormat: "F j, Y",
-//   dateFormat: "d-m-Y",
-//   "locale": French,
-// })
-
-
-if (startDateInput) {
-const unavailableDates = JSON.parse(document.querySelector('#flat-booking-dates').dataset.unavailable)
-endDateInput.disabled = true
-
-flatpickr(startDateInput, {
+flatpickr("[data-behavior='flatpickr']", {
+  altInput: true,
   minDate: "today",
-  disable: unavailableDates,
-  dateFormat: "Y-m-d",
-});
+  altFormat: "F j, Y",
+  dateFormat: "d-m-Y",
+  "locale": French,
+})
 
-console.log('im in the file')
 
-startDateInput.addEventListener("change", (e) => {
-  if (startDateInput != "") {
-    endDateInput.disabled = false
-  }
-  flatpickr(endDateInput, {
-    minDate: e.target.value,
-    disable: unavailableDates,
-    dateFormat: "Y-m-d"
-    });
-  })
-};
-
+/
 
 
 export {flatpickr};
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,16 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :room
   has_many :perks
+  validates_presence_of :date_in, :date_out
+
+# Check if a given interval overlaps this interval
+# def overlaps?(other)
+#   date_in <= other.date_out && other.date_in <= date_out
+# end
+#
+# # Return a scope for all interval overlapping the given interval, excluding the given interval itself
+# scope :overlapping, -> { |interval|
+#   where("id <> ? AND date_in <= ? AND ? <= date_out", interval.id, interval.date_out, interval.date_in)
+# }
+
 end
