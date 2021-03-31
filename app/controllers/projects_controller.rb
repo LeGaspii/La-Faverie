6dd6ec09d06c7a302820e80498f1eab8bff23256
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-03-31 14:17:15 +0200

    modal + placeholder sur les formulaires

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,7 +11,7 @@ class ProjectsController < ApplicationController
     @buying = Buying.new
     @usersproject = Usersproject.new
     @usersprojects = Usersproject.where(project_id: @project).includes([:user])
-    @usersprojects_past = @usersprojects.where("date_end < ?", Date.today)
+    @usersprojects_past = @usersprojects.where("date_end < ?", Date.today).last(5)
     @usersprojects_futur = @usersprojects.where("date_end >= ?", Date.today)
     @comment = Comment.new
     @comments = Comment.where(project_id: @project).includes([:rich_text_rich_body]).includes([:user]).last(50)
@@ -29,8 +29,8 @@ class ProjectsController < ApplicationController
   end
 
   def update
-    authorize @project
     @project = Project.find(params[:id])
+    authorize @project
     if @project.update(project_params)
       redirect_to project_path(@project)
     else
@@ -39,8 +39,8 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    authorize @project
     @project = Project.new(project_params)
+    authorize @project
     @project.house = House.find(1)
     if @project.save
       task = Task.new(project_id: @project.id, name: "example")
