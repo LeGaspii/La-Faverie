9a123c4c922bbc56837c9ccad5283db23e37e3a2
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-04-01 15:44:48 +0200

    perk policy pour pundit

--- app/controllers/perks_controller.rb
+++ app/controllers/perks_controller.rb
@@ -1,18 +1,20 @@
 class PerksController < ApplicationController
-before_action :set_perk, only: [:edit, :update, :destroy]
+  before_action :set_perk, only: [:edit, :update, :destroy]
 
   def new
-  @room = Room.find(params[:room_id])
-  @perk = Perk.new
+    @perk = Perk.new
+    authorize @perk
   end
 
   def edit
     @perk = Perk.find(params[:id])
+    authorize @perk
   end
 
   def update
     @room = Room.find(params[:room_id])
-     @perk = Perk.find(params[:id])
+    @perk = Perk.find(params[:id])
+    authorize @perk
     if @perk.update(perk_params)
       redirect_to room_path(@room)
     else
@@ -24,6 +26,7 @@ before_action :set_perk, only: [:edit, :update, :destroy]
     @room = Room.find(params[:room_id])
     @perk = Perk.new(perk_params)
     @perk.room_id = @room.id
+    authorize @perk
     if @perk.save!
       redirect_to room_path(@room)
     else
@@ -32,16 +35,18 @@ before_action :set_perk, only: [:edit, :update, :destroy]
   end
 
   def destroy
+    authorize @perk
     @perk.destroy
     redirect_to rooms_path
   end
 
   private
+
   def set_perk
     @perk = Perk.find(params[:id])
   end
+
   def perk_params
     params.require(:perk).permit(:name, :number)
   end
 end
-
