16167bd85f9c514f01ecabbd91b3c7a091e511ca
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-04-22 15:03:49 +0200

    inversement ordre pourcentage - titre sur la tab des projets

--- app/javascript/component/projects.js
+++ app/javascript/component/projects.js
@@ -2,10 +2,11 @@
 
 const projectsTitle = () => {
   const data = document.querySelector('.project-title')
+  if(data) {
   const name = data.dataset.name
   const percent = data.dataset.percent
-  document.title = `${name} - ${percent}%`
+  document.title = `${percent}% - ${name}`
+  }
 }
-
 
 export {projectsTitle};
